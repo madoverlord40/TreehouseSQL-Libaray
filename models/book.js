f8b3359cea9c1c04c52e5c8d65fedfa3ca602e35
jamesh <jamesh39@outlook.com>
2021-02-11 15:34:14 -0800

    functional, needs error page and cleanup

--- models/book.js
+++ models/book.js
@@ -1,44 +1,57 @@
 'use strict';
 
+const Sequelize = require('sequelize');
+
 const {
-  Model
+    Model
 } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
-  class Book extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  Book.init({
-    title: {
-      type: DataTypes.STRING,
-      validate: {
-        allowNull: false,
-        notEmpty: {
-          msg: '"Title" is required'
+    class Book extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
         }
-      }
-    },
-    author: {
-      type: DataTypes.STRING,
-      validate: {
-        allowNull: false,
-        notEmpty: {
-          msg: '"Author" is required'
-        }
-      }
-    },
-    genre: DataTypes.STRING,
-    year: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'Book',
-  });
-  return Book;
+    };
+    Book.init({
+        id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: {
+                    msg: '"Title" is required'
+                },
+                notNull: {
+                    msg: 'Please provide a value for "Title"',
+                },
+            }
+        },
+        author: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: {
+                    msg: '"Author" is required'
+                },
+                notNull: {
+                    msg: 'Please provide a value for "Author"',
+                },
+            }
+        },
+        genre: { type: Sequelize.STRING },
+        year: { type: Sequelize.INTEGER }
+    }, {
+        sequelize,
+        modelName: 'Book',
+    });
+    return Book;
 };
--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,130 @@
 var express = require('express');
 var router = express.Router();
-var BookModel = require('../models/book');
+var BookModel = require('../models').Book;
+
+/* Handler function to wrap each route. */
+function asyncHandler(cb) {
+    return async(req, res, next) => {
+        try {
+            await cb(req, res, next)
+        } catch (error) {
+            // Forward error to the global error handler
+            next(error);
+        }
+    }
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  //res.render('index', { title: 'Express' });
 
-  fetchBooks();
+    fetchBooks();
 
+    //res.render('index', { title: 'Express' });
+    res.redirect('/Books');
 });
 
-fetchBooks = async () => {
-  
-  try {
-    // ... All model instances
+router.get('/Books', asyncHandler(async(req, res) => {
+    const books = await BookModel.findAll();
 
-    const Books = await BookModel.findAll();
-    console.log( Books.map(Books => Books.toJSON()) );
+    res.render("index", { books });
+}));
 
-  } catch(error) {
-    console.log(error);
-  }
+router.get('/Books/new', (req, res, next) => {
+
+    res.render('new');
+});
+
+router.post('/Books', asyncHandler(async(req, res) => {
+    const result = await addModifyBook(req.body);
+
+    if (result.success) {
+        res.redirect("/Books");
+    } else {
+        res.render("formerror", { newBook: result.data, errors: result.errors })
+    }
+}));
+
+router.get('/Books/:id', asyncHandler(async(req, res, next) => {
+    const book = await BookModel.findByPk(req.params.id);
+    if (book) {
+        res.render("details", { book });
+    } else {
+        res.sendStatus(404);
+    }
+}));
+
+router.post("/Books/:id/edit", asyncHandler(async(req, res, next) => {
+    const book = await BookModel.findByPk(req.params.id);
+    if (book) {
+        result = await addModifyBook(req.body, book);
+
+        if (result.success) {
+            res.redirect("/Books");
+        }
+    } else {
+        res.sendStatus(404);
+    }
+}));
+
+router.post('/Books/:id/delete', asyncHandler(async(req, res, next) => {
+    try {
+        const book = await BookModel.findByPk(req.params.id);
+        if (book) {
+            BookModel.destroy({
+                where: {
+                    id: book.id
+                }
+            });
+            res.redirect("/Books");
+
+        } else {
+            res.sendStatus(404);
+        }
+
+    } catch (error) {
+
+    }
+}));
+
+fetchBooks = async() => {
+
+    try {
+        // ... All model instances
+
+        const Books = await BookModel.findAll();
+        console.log(Books.map(Books => Books.toJSON()));
+
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+addModifyBook = async(formData, bookObject = null) => {
+    let bookData = {
+        title: formData.title,
+        author: formData.author,
+        genre: formData.genre,
+        year: formData.year
+    };
+
+    try {
+        if (bookObject !== null) {
+            newBook = await bookObject.update(bookData, {
+                where: { id: bookObject.id }
+            });
+            return { data: newBook, errors: [], success: true };
+        } else {
+            newBook = await BookModel.create(bookData);
+            return { data: newBook, errors: [], success: true };
+        }
+    } catch (error) {
+        if (error.name === "SequelizeValidationError") {
+            newBook = await BookModel.build(data);
+            return { data: newBook, errors: error.errors, success: false };
+        } else {
+            throw error;
+        }
+    }
 }
 
 module.exports = router;
